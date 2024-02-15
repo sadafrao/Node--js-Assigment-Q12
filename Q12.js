6ca98ab5df77c7d02fe05b0e376e95ff42444f0e
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:35:55 -0800

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,8 @@
+// Q12 : Greething : Start with the array you used in Exercise 11, but instead of just Printing each Persons name Print a message to them
+// The Text of each message to be the same , but each messge should be personalized with the person name .
+//Q12
+var names = ["sadaf", "sana", "safina", "zara"];
+var message = " How are you Dear";
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
